dd216ede747375174e953ebd3b74156c5bad0c3c
sakshinarute <sakshi_narute@epam.com>
2025-10-30 17:43:21 +0530

    Updated main class for MCP test

--- src/main/java/org/epam/mcp/Main.java
+++ src/main/java/org/epam/mcp/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello from MCP branch");
         System.out.println("Checking PR into main by github copilot");
+        System.out.println("Hello from MCP integrated workflow!");
     }
 }
